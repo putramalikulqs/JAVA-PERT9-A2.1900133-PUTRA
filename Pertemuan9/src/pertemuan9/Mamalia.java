2979f3f0445c27475556b55cdd347805936ff135
sandy somantri <azidanm8@gmail.com>
2019-11-14 11:29:26 +0700

    Pertemuan 9

---
+++ Pertemuan9/src/pertemuan9/Mamalia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author 
+ * Nama : Putra Malikul Quddus Sunjaya
+ * Kelas : TI-1A
+ * NIM : A2.1900133
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
